5b8da368d0af0b374267448cdf4349b83016e22c
Marci Taylor <Marci.taylor@libertymutual.com>
2018-01-09 16:27:17 -0500

    Updating Routing

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { IngredientComponent } from '../ingredient/ingredient.component';
 import { IngredientFormComponent } from '../ingredient-form/ingredient-form.component';
+import { RecipesComponent } from '../recipes/recipes.component';
+import { RecipesFormComponent } from '../recipes-form/recipes-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent },
   { path: 'ingredient',  component: IngredientComponent },
   { path: 'ingredient/add', component: IngredientFormComponent },
+  { path: 'recipe',  component: RecipesComponent },
+  { path: 'recipe/edit/:id', component: RecipesComponent },
+  { path: 'recipe/add', component: RecipesFormComponent },
 ];
 
 @NgModule({
