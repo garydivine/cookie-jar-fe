a9b0f0838ebd93bbf244d172978e913b515e4107
Karen Alyea <karen.alyea@libertymutual.com>
2018-01-09 14:28:06 -0500

    adding ingredient components

--- src/app/ingredient-form/ingredient-form.component.ts
+++ src/app/ingredient-form/ingredient-form.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { fadeInAnimation } from '../animations/fade-in.animation';
+import { NgForm} from '@angular/forms';
+import { DataService } from '../data.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
 
 @Component({
   selector: 'app-ingredient-form',
   templateUrl: './ingredient-form.component.html',
-  styleUrls: ['./ingredient-form.component.css']
+  styleUrls: ['./ingredient-form.component.css'],
+  animations: [fadeInAnimation]
 })
 export class IngredientFormComponent implements OnInit {
 
-  constructor() { }
+  ingredientForm: NgForm;
+  @ViewChild('ingredientForm')
+  currentForm: NgForm;
+
+  successMessage: string;
+  errorMessage: string;
+
+  ingredient: object;
+
+
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  getRecordsForEdit() {
+    this.route.params
+    .switchMap((params: Params) => this.dataService.getRecord("ingredients", +params['id']))
+    .subscribe(ingredient => this.ingredient = ingredient);
+  }
 
   ngOnInit() {
+    this.route.params
+    .subscribe((params: Params) => {
+      (+params['id']) ?this.getRecordsForEdit() : null;
+    });
+  }
+
+  saveIngredient(ingredientForm: NgForm) {
+    if(typeof ingredientForm.value.id === "number") {
+      this.dataService.editRecord("ingredient",ingredientForm.value, ingredientForm.value.id)
+      .subscribe(
+        ingredient => this.successMessage = "Record updated",
+        error => this.errorMessage = <any>error);
+      }
+
+
+
+    }
   }
 
 }
--- src/app/ingredient/ingredient.component.ts
+++ src/app/ingredient/ingredient.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { fadeInAnimation } from '../animations/fade-in.animation';
+import { DataService } from '../data.service';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
 @Component({
   selector: 'app-ingredient',
   templateUrl: './ingredient.component.html',
-  styleUrls: ['./ingredient.component.css']
+  styleUrls: ['./ingredient.component.css'],
+  animations: [fadeInAnimation]
 })
-export class IngredientComponent implements OnInit {
 
-  constructor() { }
+export class IngredientComponent implements OnInit {
+  errorMessage: string;
+  successMessage: string;
+  ingredients: any[];
+
+  constructor(private dataService: DataService, public dialog: MatDialog) { }
+  
+  getIngredients() {
+    this.dataService.getRecords("ingredients").subscribe(
+      ingredients => this.ingredients = ingredients,
+      error => this.errorMessage = <any>error);
+  }
+
+  deleteIngredient(id:number) {
+    let dialogRef = this.dialog.open(DeleteConfirmComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result){this.dataService.deleteRecord("ingredients", id)
+      .subscribe( ingredients => {
+        this.successMessage = "Record(s) deleted" ;
+        this.getIngredients();
+        },
+      error => this.errorMessage = <any>error);
+      }
+    });
+  }
 
   ngOnInit() {
+    this.getIngredients();
   }
 
 }
