a3088918a13ac9aecc1a421f4180f77a2e6011fa
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-09 12:50:21 -0500

    Updating Routing

--- src/app/routing/routing.component.ts
+++ src/app/routing/routing.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { IngredientComponent } from '../ingredient/ingredient.component';
+import { IngredientFormComponent } from '../ingredient-form/ingredient-form.component';
 
-@Component({
-  selector: 'app-routing',
-  templateUrl: './routing.component.html',
-  styleUrls: ['./routing.component.css']
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',  component: HomeComponent },
+  { path: 'ingredient',  component: IngredientComponent },
+  { path: 'ingredient/add', component: IngredientFormComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class RoutingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class AppRoutingModule {}
