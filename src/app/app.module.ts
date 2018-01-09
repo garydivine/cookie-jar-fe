b121ad943b164b44968dc887bb1db82ab9c88acc
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-09 12:43:36 -0500

    Updating Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DeleteConfimComponent } from './delete-confim/delete-confim.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RoutingComponent } from './routing/routing.component';
+import { StatusMessageComponent } from './status-message/status-message.component';
+import { AnimationsComponent } from './animations/animations.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteConfimComponent,
+    HomeComponent,
+    NavigationComponent,
+    RoutingComponent,
+    StatusMessageComponent,
+    AnimationsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/delete-confim/delete-confim.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confim',
+  templateUrl: './delete-confim.component.html',
+  styleUrls: ['./delete-confim.component.css']
+})
+export class DeleteConfimComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
