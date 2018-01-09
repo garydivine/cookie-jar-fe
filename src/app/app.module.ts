3231d1c2dd070a1646b2cb255b2874134beb45fd
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-09 13:11:51 -0500

    Updating App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { MatDialogModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { DeleteConfimComponent } from './delete-confim/delete-confim.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { StatusMessageComponent } from './status-message/status-message.component';
 import { DataService } from './data.service';
-// import { AppRoutingModule } from './routing/routing.module';
-// import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
+import { AppRoutingModule } from './routing/routing.module';
+import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
 
 import { IngredientComponent } from './ingredient/ingredient.component';
 import { IngredientFormComponent } from './ingredient-form/ingredient-form.component';
@@ -22,17 +22,17 @@ import { IngredientFormComponent } from './ingredient-form/ingredient-form.compo
     AppComponent,
     NavigationComponent,
     HomeComponent,
-    // DeleteConfirmComponent,
+    DeleteConfirmComponent,
     StatusMessageComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
+    AppRoutingModule,
     HttpModule,
     BrowserAnimationsModule,
     FormsModule
   ],
-  // entryComponents: [DeleteConfirmComponent],
+  entryComponents: [DeleteConfirmComponent],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
