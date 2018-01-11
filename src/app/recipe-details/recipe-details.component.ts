e64767fd0696e1621ffc61182d6dbd47dde140a6
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-11 13:16:48 -0500

    Adding Recipe Details Functionaity

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
+
+  @Input() recipeDetails: any[];
 
   constructor() { }
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
-import { DataService } from '../data.service'
+import { DataService } from '../data.service';
 import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
 import { fadeInAnimation } from '../animations/fade-in.animation';
 
@@ -15,35 +15,43 @@ export class RecipesComponent implements OnInit {
   errorMessage: string;
   successMessage: string;
   recipes: any[];
+  recipeDetails;
 
   constructor(private dataService: DataService, public dialog: MatDialog) { }
 
   getRecipes() {
-    this.dataService.getRecords("recipes")
+    this.dataService.getRecords('recipes')
       .subscribe(
         recipes => this.recipes = recipes,
         error =>  this.errorMessage = <any>error);
   }
 
-  deleteRecipe(id:number) {
+  getRecipeDetails(id: number) {
+    this.dataService.getRecord('recipes', id)
+      .subscribe(
+        recipeDetails => this.recipeDetails = recipeDetails,
+        error =>  this.errorMessage = <any>error);
+  }
 
-    let dialogRef = this.dialog.open(DeleteConfirmComponent);
+  deleteRecipe(id: number) {
+
+    const dialogRef = this.dialog.open(DeleteConfirmComponent);
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        this.dataService.deleteRecord("recipes", id)
+      if (result) {
+        this.dataService.deleteRecord('recipes', id)
           .subscribe(
             recipes => {
-              this.successMessage = "Record(s) deleted successfully"; 
-              this.getRecipes(); 
+              this.successMessage = 'Record(s) deleted successfully';
+              this.getRecipes();
             },
             error =>  this.errorMessage = <any>error);
       }
     });
   }
 
-  ngOnInit() { 
-    this.getRecipes(); 
+  ngOnInit() {
+    this.getRecipes();
   }
 
 }
