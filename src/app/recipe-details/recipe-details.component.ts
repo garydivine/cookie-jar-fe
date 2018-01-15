f4985cf3289cbf183865e72850f1dfde7bc254a4
Gary Divine <gary.divine@libertymutual.com>
2018-01-15 14:25:06 -0500

    Email Recipe Functionality

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -8,9 +8,39 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 })
 export class RecipeDetailsComponent implements OnInit {
 
+  instructions: string;
+
+  linkForEmail: string = `mailto:test@example.com&subject=${this.data.recipeDetails.name}&body=Bake at ${this.data.recipeDetails.temp} for ${ this.data.recipeDetails.time}.
+%0D
+%0D
+Instructions: 
+%0D
+${this.data.recipeDetails.instructions}
+%0D
+%0D
+Yield:
+%0D
+${this.data.recipeDetails.yield}
+%0D
+%0D
+Ingredients:
+%0D
+`
+
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+  }
+
+  openUserMailClient(){
+    for (let ingredientItem of this.data.recipeDetails.ingredientRecipeListItem) {
+      this.linkForEmail = this.linkForEmail + `${ingredientItem.quantity} ${ingredientItem.unitOfMeasurement} of ${ingredientItem.ingredient.name}%0D`
+    }
+
+
+
+    window.location.href = this.linkForEmail;
   }
 
 }
