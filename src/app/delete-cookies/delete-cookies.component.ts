eff8bee2527debc8eeb23c997dac643e1d231d3b
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-18 11:03:22 -0500

    Updating Delete Cookies Button

---
+++ src/app/delete-cookies/delete-cookies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-cookies',
+  templateUrl: './delete-cookies.component.html',
+  styleUrls: ['./delete-cookies.component.css']
+})
+export class DeleteCookiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
