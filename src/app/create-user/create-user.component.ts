af19944f9fb2ecb6847abc244791b8eb600d4534
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-22 18:56:29 -0500

    Updating Create User with PW check

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { fadeInAnimation } from '../animations/fade-in.animation';
 import { UserService } from '../user.service';
+import { LoginService } from '../login.service';
 import { NgForm } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
@@ -16,20 +17,35 @@ export class CreateUserComponent implements OnInit {
   successMessage: string;
   errorMessage: string;
   user: any;
+  loginInfo: any;
 
   @Output() loginRequestSubmitted = new EventEmitter();
 
   constructor(
     private userService: UserService,
+    private loginService: LoginService,
     public dialogRef: MatDialogRef<CreateUserComponent>
   ) { }
 
   createUser(userForm: NgForm) {
     this.errorMessage = '';
+    if (userForm.value.password !== userForm.value.confirmPassword) {
+      this.errorMessage = 'Passwords do not match';
+    } else {
     this.userService.createUser('create', userForm.value)
       .subscribe(
       user => {
         this.dialogRef.close();
+        localStorage.setItem('user', JSON.stringify(user));
+      },
+      error => this.errorMessage = <any>error);
+    this.loginInfo = {
+      'user' : this.user.username,
+      'password' : this.user.password
+    };
+    this.loginService.loginUser('login', this.loginInfo)
+      .subscribe(
+      user => {
         localStorage.setItem('user', JSON.stringify(user));
       },
       error => this.errorMessage = <any>error);
@@ -38,6 +54,7 @@ export class CreateUserComponent implements OnInit {
       result => {
         location.reload();
       });
+    }
   }
 
   afterClosed() {
