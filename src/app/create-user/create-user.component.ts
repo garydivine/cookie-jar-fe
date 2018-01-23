f28e13eb63ea32d54c3363d8bba287091667209c
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-23 13:28:55 -0500

    Adding Form Validation to Create User

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { fadeInAnimation } from '../animations/fade-in.animation';
 import { UserService } from '../user.service';
 import { LoginService } from '../login.service';
@@ -13,11 +13,14 @@ import { MatDialog, MatDialogRef } from '@angular/material';
 })
 export class CreateUserComponent implements OnInit {
 
-  userForm: NgForm;
   successMessage: string;
   errorMessage: string;
   user: any;
   loginInfo: any;
+
+  userForm: NgForm;
+  @ViewChild('userForm')
+  currentForm: NgForm;
 
   @Output() loginRequestSubmitted = new EventEmitter();
 
@@ -53,5 +56,62 @@ export class CreateUserComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    this.loginInfo = this.currentForm;
+    this.loginInfo.valueChanges
+      .subscribe(
+        data => this.onValueChanged()
+      );
+  }
+
+  onValueChanged() {
+    const form = this.loginInfo.form;
+
+    // tslint:disable-next-line:forin
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        // tslint:disable-next-line:forin
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  // tslint:disable-next-line:member-ordering
+  formErrors = {
+    'firstName': '',
+    'lastName': '',
+    'username': '',
+    'password': ''
+  };
+
+  // tslint:disable-next-line:member-ordering
+  validationMessages = {
+    'firstName': {
+      'required': 'First Name is required',
+    },
+    'lastName': {
+      'required': 'Last Name is required',
+    },
+    'username': {
+      'required': 'Username is required',
+    },
+    'password': {
+      'required': 'Password is required',
+    }
+
+  };
 
 }
